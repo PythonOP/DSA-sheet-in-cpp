2426e031c085c97f17cc9cac6923ad300b890d1c
Soumyajit Saha <67553065+PythonOP@users.noreply.github.com>
2023-10-25 23:57:49 +0530

    tests

--- file.cpp
+++ file.cpp
@@ -3,29 +3,23 @@
 #include<algorithm>
 #include<limits.h>
 using namespace std;
-// Example 1:
-// Input: 1 ,0 ,2 ,3 ,0 ,4 ,0 ,1
-// Output: 1 ,2 ,3 ,4 ,1 ,0 ,0 ,0
-// Explanation: All the zeros are moved to the end and non-negative integers are moved to front by maintaining order
-vector<int> moveZeros(int n, vector<int> a) {
-    // Brute force approach - Initial
-    for(int i=n-1;i>0;i--){
-        if(a[i-1] == 0){
-            a.erase(a.begin()+(i-1));
-            a.push_back(0);
-        }
-    }
-    return a;
-}
+
 int main(){
-    int n;
-    cin>>n;
-    vector<int> a(n);
-    for(int i=0;i<n;i++)    cin>>a[i];
-    // for(int i=0;i<n;i++)    cout<<a[i];
-    vector<int> res = moveZeros(n,a);
-    for(auto it:res)    cout<<it<<" ";
-    // cout<<"Test";
+    // int n;
+    // cin>>n;
+    // vector<int> a(n);
+    // for(int i=0;i<n;i++)    cin>>a[i];
+    // // for(int i=0;i<n;i++)    cout<<a[i];
+    // vector<int> res = moveZeros(n,a);
+    // for(auto it:res)    cout<<it<<" ";
+    // // cout<<"Test";<<
+
+    int a=5,b=10;
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+
+    cout<<a<<" "<<b;
 }
 // Left Rotate the Array by One
 // https://www.codingninjas.com/studio/problems/left-rotate-an-array-by-one_5026278
