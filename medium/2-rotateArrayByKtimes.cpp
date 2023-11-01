5ce58dd43c8667e4d801f095566907f52d7fdb9a
Soumyajit Saha <67553065+PythonOP@users.noreply.github.com>
2023-11-01 21:19:39 +0530

    optimised solution added

--- medium/2-rotateArrayByKtimes.cpp
+++ medium/2-rotateArrayByKtimes.cpp
@@ -3,6 +3,7 @@ using namespace std;
 class Solution {
 public:
     // void rotate(vector<int>& nums, int k) {
+    //     //Initial Approach
     //     k = k % nums.size();
     //     int n=nums.size();
     //     for(int f=0;f<k;f++){
@@ -15,18 +16,32 @@ public:
         
     //     for(auto it:nums)   cout<<it<<" ";
     // }
+    // void rotate(vector<int>& nums, int k) {
+    //     // Better approach using extra vector
+    //     int n=nums.size();
+    //     vector<int> a(n);
+        
+    //     for (int i = 0; i < n; i++) {
+    //         a[(i + k) % nums.size()] = nums[i];
+    //     }
+    //     for (int i = 0; i < n; i++) {
+    //         nums[i] = a[i];
+    //     }
+        
+    //     for(auto it:nums)   cout<<it<<" ";
+    // }
     void rotate(vector<int>& nums, int k) {
+        // Best solution with O(n) time and O(1) space
         int n=nums.size();
-        vector<int> a(n);
-        
-        for (int i = 0; i < n; i++) {
-            a[(i + k) % nums.size()] = nums[i];
-        }
-        for (int i = 0; i < n; i++) {
-            nums[i] = a[i];
-        }
-        
+        // sort doesn't work because we don't want to change the order of the values in test cases
+        // sort(nums.begin(),nums.end(),greater<int>());
+        // sort(nums.begin(),nums.begin()+k);
+        // sort(nums.begin()+k,nums.end());
+
+        k = k % n; //if k>=n
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
         for(auto it:nums)   cout<<it<<" ";
     }
 };
-// https://leetcode.com/problems/rotate-array/description/
