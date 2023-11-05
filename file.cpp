fb43a542eb4357f97c0386830d2bf63cd68537d8
Soumyajit Saha <67553065+PythonOP@users.noreply.github.com>
2023-11-05 23:16:11 +0530

    tests

--- file.cpp
+++ file.cpp
@@ -7,7 +7,7 @@ int main(){
     vector<int> a(n);
     for(int i=0;i<n;i++)    cin>>a[i];
     // for(int i=0;i<n;i++)    cout<<a[i];
-    vector<int> res = sortedArray(a,b);
-    for(auto it:res)    cout<<it<<" ";
+    // vector<int> res = sortedArray(a,b);
+    // for(auto it:res)    cout<<it<<" ";
     // cout<<"Test";
 }
