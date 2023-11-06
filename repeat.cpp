9ca7791000ef0e7193d35314f9eb5ac7d9b650f7
Soumyajit Saha <67553065+PythonOP@users.noreply.github.com>
2023-11-06 22:22:29 +0530

    tests

---
+++ repeat.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int repeatingEle(vector<int> a){
+    int n = a.size();
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+            if(a[i]==a[j]){
+                return a[i];
+            }
+        }
+    }
+    return -1;
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++)    cin>>a[i];
+    for(int i=0;i<n;i++)    cout<<a[i];
+    cout<<"\nRepeating Element:"<<repeatingEle(a);
+}
+// Find the first repeating element in an array of integers
