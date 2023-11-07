35d4dd87d30f05740e26d947bf73cf96f318031d
Soumyajit Saha <67553065+PythonOP@users.noreply.github.com>
2023-11-07 23:57:59 +0530

    optimised

--- medium/4-MaxSubarray.cpp
+++ medium/4-MaxSubarray.cpp
@@ -18,16 +18,35 @@ public:
     // }
 
     //Better Solution
-    int maxSubArray(vector<int>& nums) {
-        int Max = INT_MIN;
-        for(int i=0;i<nums.size();i++){
-            int sum=0;
-            for(int j=i;j<nums.size();j++){
-                sum += nums[j];
-                Max = max(sum,Max);
-            }
+    // int maxSubArray(vector<int>& nums) {
+    //     int Max = INT_MIN;
+    //     for(int i=0;i<nums.size();i++){
+    //         int sum=0;
+    //         for(int j=i;j<nums.size();j++){
+    //             sum += nums[j];
+    //             Max = max(sum,Max);
+    //         }
             
+    //     }
+    //     return Max;
+    // }
+    int maxSubArray(vector<int>& nums) {
+        long long maxi = LONG_MIN; // maximum sum
+        long long sum = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            sum += nums[i];
+
+            if (sum > maxi) {
+                maxi = sum;
+            }
+
+            
+            if (sum < 0) {
+                sum = 0;
+            }
         }
-        return Max;
-    }
+        return maxi;
+        }
 };
