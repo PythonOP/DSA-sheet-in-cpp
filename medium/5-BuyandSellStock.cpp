c274127b854337b036a48806a1aafd3c14ded75a
Soumyajit Saha <67553065+PythonOP@users.noreply.github.com>
2023-11-08 21:30:43 +0530

    initial approach

---
+++ medium/5-BuyandSellStock.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        //Initial Approach
+        // find lowest along with index
+        // find largest on its right side
+
+        //Failed test cases
+        int low=INT_MAX;
+        int lowIndex;
+        for(int i=0;i<prices.size();i++){
+            if(prices[i]<low){lowIndex=i;low=min(prices[i],low);};
+        }
+        int high=INT_MIN;
+        int highIndex;
+        for(int i=lowIndex;i<prices.size();i++){
+            if(prices[i]>high){highIndex=i;high=max(prices[i],high);}
+        }
+        
+        return (high-low);
+        
+    }
+};
