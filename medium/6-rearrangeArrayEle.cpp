e3900c46bf80c0389974c2b7be34a0487825f07d
Soumyajit Saha <67553065+PythonOP@users.noreply.github.com>
2023-11-09 21:37:29 +0530

    optimal solution updated

--- medium/6-rearrangeArrayEle.cpp
+++ medium/6-rearrangeArrayEle.cpp
@@ -23,4 +23,20 @@ public:
         }
         return nums;
     }
+    // optimal solution
+    vector<int> rearrangeArray(vector<int>& nums) {
+       vector<int> ans(nums.size());
+       int posIdx=0,negIdx=1;
+       for(int i=0;i<nums.size();i++){
+           if(nums[i]>0){
+               ans[posIdx] = nums[i];
+               posIdx += 2;
+           }
+           else{
+               ans[negIdx] = nums[i];
+               negIdx += 2;
+           }
+       }
+       return ans;
+    }
 };
