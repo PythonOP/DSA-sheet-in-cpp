7b9e69e09215729ac1719fe74df1add46f54751e
Soumyajit Saha <67553065+PythonOP@users.noreply.github.com>
2023-11-09 20:21:44 +0530

    optimised loop

--- medium/6-rearrangeArrayEle.cpp
+++ medium/6-rearrangeArrayEle.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 class Solution {
 public:
-    // Initial Approach
+    //Initial Approach with optimised loop
     vector<int> rearrangeArray(vector<int>& nums) {
         vector<int> pos;
         vector<int> neg;
@@ -10,10 +10,16 @@ public:
             if(nums[i]>0){pos.push_back(nums[i]);}
             else{neg.push_back(nums[i]);}
         }
-        int j=0,k=0;
-        for(int i=0;i<nums.size();i++){
-            if(i%2 == 0){nums[i]=pos[j++];}
-            else{nums[i]=neg[k++];}
+        // int j=0,k=0;
+        // for(int i=0;i<nums.size();i++){
+        //     if(i%2 == 0){nums[i]=pos[j++];}
+        //     else{nums[i]=neg[k++];}
+        // }
+        
+        // Optimised loop 
+        for(int i=0;i<nums.size()/2;i++){
+            nums[2*i]=pos[i];
+            nums[2*i+1]=neg[i];
         }
         return nums;
     }
