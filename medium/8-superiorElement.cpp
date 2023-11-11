72b733a2a5dc159a77b99669aee6dda0ad9da061
Soumyajit Saha <67553065+PythonOP@users.noreply.github.com>
2023-11-11 23:53:59 +0530

    optimal solution updated

--- medium/8-superiorElement.cpp
+++ medium/8-superiorElement.cpp
@@ -18,6 +18,23 @@ vector<int> superiorElements(vector<int>&a) {
     sort(ans.begin(),ans.end());
     return ans;
 }
+
+// Optimal Solution
+vector<int> superiorElements(vector<int>&a) {
+    int n=a.size();
+    vector<int> ans;
+
+    int max=a[n-1];
+    ans.push_back(a[n-1]);
+
+    for(int i=n-2;i>=0;i--){
+      if(a[i]>max){
+        ans.push_back(a[i]);
+        max = a[i];
+      }
+    }
+    return ans;
+}
 int main(){
     int n;
     cin>>n;
